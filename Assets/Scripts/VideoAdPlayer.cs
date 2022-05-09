0426318f929afb1a09ae0e7f6ff08203ae21c5ca
Luna Orlata <the.keksik.is.a.lie@gmail.com>
2022-05-09 14:17:37 +0300

    fixed video ad player aspect ratio

--- Assets/Scripts/VideoAdPlayer.cs
+++ Assets/Scripts/VideoAdPlayer.cs
@@ -7,28 +7,38 @@ public class VideoAdPlayer : Singleton<VideoAdPlayer>
     private VideoPlayer player;
     private GameObject panel;
     private RawImage image;
+    private AspectRatioFitter ratioFitter;
 
     private void Start()
     {
-        Transform content = transform.Find("Panel/VideoContent");
+        Transform content = transform.Find("Panel/VideoContainer/VideoContent");
         player = content.GetComponent<VideoPlayer>();
         player.loopPointReached += OnPlaybackCompleted;
         player.prepareCompleted += OnPrepareCompleted;
 
         image = content.GetComponent<RawImage>();
+        ratioFitter = content.GetComponent<AspectRatioFitter>();
         panel = transform.Find("Panel").gameObject;
+
+        panel.SetActive(false);
     }
 
     public void PlayAd(string url)
     {
         panel.SetActive(true);
+        image.enabled = false;
+
         player.url = url;
         player.Prepare();
     }
 
     private void OnPrepareCompleted(VideoPlayer source)
     {
-        image.texture = player.texture;
+        image.enabled = true;
+        Texture texture = player.texture;
+
+        image.texture = texture;
+        ratioFitter.aspectRatio = (float)texture.width / (float)texture.height;
         player.Play();
     }
 
